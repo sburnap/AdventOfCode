76a8b62d5f08d2e944177d1d47f3ba64528ee025
Steve Burnap <steve@burnap.net>
2022-12-08 22:06:49 -0800

    2015 Day 9

--- Advent2022/Day9/Day9.py
+++ Advent2022/Day9/Day9.py
@@ -20,36 +20,35 @@ class Location:
 
 
 def do_moves(moves: list[Move], length: int = 2) -> int:
-    head = Location(0, 0)
-    tail = Location(0, 0)
-
-    rope = [Location(0, 0)] * length
 
     visited: set[str] = set()
+    rope = [Location(0, 0) for _ in range(length)]
 
     for move in moves:
         for _ in range(move.distance):
             match move.direction:
                 case "U":
-                    head.y -= 1
+                    rope[0].y -= 1
                 case "D":
-                    head.y += 1
+                    rope[0].y += 1
                 case "L":
-                    head.x -= 1
+                    rope[0].x -= 1
                 case "R":
-                    head.x += 1
+                    rope[0].x += 1
 
-            diffy = head.y - tail.y
-            diffx = head.x - tail.x
-            if abs(diffy) >= 2 and abs(diffx) == 0:
-                tail.y += diffy // abs(diffy)
-            elif abs(diffx) >= 2 and abs(diffy) == 0:
-                tail.x += diffx // abs(diffx)
-            elif abs(diffy) >= 2 or abs(diffx) >= 2:
-                tail.y += diffy // abs(diffy)
-                tail.x += diffx // abs(diffx)
+            for i in range(len(rope) - 1):
+                diffy = rope[i].y - rope[i + 1].y
+                diffx = rope[i].x - rope[i + 1].x
 
-            visited.add(f"{tail.x},{tail.y}")
+                if abs(diffy) >= 2 and abs(diffx) == 0:
+                    rope[i + 1].y += diffy // abs(diffy)
+                elif abs(diffx) >= 2 and abs(diffy) == 0:
+                    rope[i + 1].x += diffx // abs(diffx)
+                elif abs(diffy) >= 2 or abs(diffx) >= 2:
+                    rope[i + 1].y += diffy // abs(diffy)
+                    rope[i + 1].x += diffx // abs(diffx)
+
+            visited.add(f"{rope[-1].x},{rope[-1].y}")
 
     return len(visited)
 
@@ -58,16 +57,16 @@ def test_one(moves: list[Move]) -> Optional[int]:
     return do_moves(moves)
 
 
-def part_one(moves: list[str]) -> Optional[int]:
+def part_one(moves: list[Move]) -> Optional[int]:
     return do_moves(moves)
 
 
-def test_two(input: str) -> Optional[int]:
-    return None
+def test_two(moves: list[Move]) -> Optional[int]:
+    return do_moves(moves, 10)
 
 
-def part_two(input: list[str]) -> Optional[int]:
-    return None
+def part_two(moves: list[Move]) -> Optional[int]:
+    return do_moves(moves, 10)
 
 
 if __name__ == "__main__":
