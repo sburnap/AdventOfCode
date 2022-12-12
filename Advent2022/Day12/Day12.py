a7ecd23ce244c70d5cd899ed74483f47f045dd1d
Steve Burnap <steve@burnap.net>
2022-12-12 07:30:32 -0800

    Now with actual Dijkstra

--- Advent2022/Day12/Day12.py
+++ Advent2022/Day12/Day12.py
@@ -1,20 +1,27 @@
-from typing import Optional, Generator
+from typing import Optional, Generator, Callable
+from dataclasses import dataclass
 
 import aoc_utils as au
 
 Map = list[list[str]]
 
 
-def adjacent(x: int, y: int, map: Map) -> Generator[tuple[int, int], None, None]:
-    if x > 0:
-        yield x - 1, y
-    if y > 0:
-        yield x, y - 1
+@dataclass
+class Position:
+    x: int
+    y: int
 
-    if x < len(map[0]) - 1:
-        yield x + 1, y
-    if y < len(map) - 1:
-        yield x, y + 1
+
+def adjacent(position: Position, map: Map) -> Generator[Position, None, None]:
+    if position.x > 0:
+        yield Position(position.x - 1, position.y)
+    if position.y > 0:
+        yield Position(position.x, position.y - 1)
+
+    if position.x < len(map[0]) - 1:
+        yield Position(position.x + 1, position.y)
+    if position.y < len(map) - 1:
+        yield Position(position.x, position.y + 1)
 
 
 def find_start_end(map: Map) -> tuple[tuple[int, int], tuple[int, int]]:
@@ -30,77 +37,79 @@ def find_start_end(map: Map) -> tuple[tuple[int, int], tuple[int, int]]:
     return start, end
 
 
-def dijstra(map: Map, start: tuple[int, int], target: tuple[int, int]) -> int:
+WalkTest = Callable[[str, str], bool]
+
+
+def dijstra(map: Map, start: Position, target: str, can_walk: WalkTest) -> int:
     dij: list[list[Optional[int]]] = [
         [None for _ in range(len(map[0]))] for _ in range(len(map))
     ]
 
-    startx, starty = start
-    dij[starty][startx] = 0
+    dij[start.y][start.x] = 0
+    work_queue: list[Position] = [start]
 
-    while True:
-        for y in range(len(map)):
-            for x in range(len(map[0])):
-                if (x, y) == target and dij[y][x] is not None:
-                    return dij[y][x]
-                for x1, y1 in adjacent(x, y, map):
-                    if (
-                        dij[y][x] is not None
-                        and dij[y1][x1] is None
-                        and ord(map[y][x]) + 1 >= ord(map[y1][x1])
-                    ):
-                        dij[y1][x1] = dij[y][x] + 1
+    while len(work_queue) > 0:
+        work = work_queue.pop(0)
+        for adj in adjacent(work, map):
+            if dij[adj.y][adj.x] is None and can_walk(
+                map[work.y][work.x], map[adj.y][adj.x]
+            ):
+                steps = dij[work.y][work.x]
+                dij[adj.y][adj.x] = steps + 1 if steps is not None else 0
+                work_queue.append(Position(adj.x, adj.y))
+                if map[adj.y][adj.x] == target:
+                    return dij[adj.y][adj.x]
+
+    return 0
+
+
+def find_start(map: Map, start: str) -> Position:
+    for y in range(len(map)):
+        for x in range(len(map[0])):
+            if map[y][x] == start:
+                return Position(x, y)
+
+    raise Exception("No starting position found")
+
+
+def can_walk_up(first: str, second: str) -> bool:
+    if first == "S":
+        first = "a"
+    if second == "E":
+        second = "z"
+    return ord(first) + 1 >= ord(second)
 
 
 def test_one(map: Map) -> Optional[int]:
-    start, target = find_start_end(map)
-    return dijstra(map, start, target)
+    start = find_start(map, "S")
+
+    return dijstra(map, start, "E", can_walk_up)
 
 
 def part_one(map: Map) -> Optional[int]:
-    start, target = find_start_end(map)
-    return dijstra(map, start, target)
+    start = find_start(map, "S")
+
+    return dijstra(map, start, "E", can_walk_up)
 
 
-def dijstra2(map: Map, start: tuple[int, int]) -> int:
-    dij: list[list[Optional[int]]] = [
-        [None for _ in range(len(map[0]))] for _ in range(len(map))
-    ]
-
-    startx, starty = start
-    dij[starty][startx] = 0
-
-    done = False
-    while not done:
-        done = True
-        for y in range(len(map)):
-            for x in range(len(map[0])):
-                for x1, y1 in adjacent(x, y, map):
-                    if (
-                        dij[y][x] is not None
-                        and dij[y1][x1] is None
-                        and ord(map[y1][x1]) + 1 >= ord(map[y][x])
-                    ):
-                        dij[y1][x1] = dij[y][x] + 1
-                        done = False
-
-    mn = 9999
-    for y in range(len(map)):
-        for x in range(len(map[0])):
-            if map[y][x] == "a" and dij[y][x] is not None and dij[y][x] < mn:
-                mn = dij[y][x]
-
-    return mn
+def can_walk_down(first: str, second: str) -> bool:
+    if first == "E":
+        first = "z"
+    if second == "S":
+        second = "a"
+    return ord(second) +1 >= ord(first) 
 
 
 def test_two(map: Map) -> Optional[int]:
-    _, target = find_start_end(map)
-    return dijstra2(map, target)
+    start = find_start(map, "E")
+
+    return dijstra(map, start, "a", can_walk_down)
 
 
 def part_two(map: Map) -> Optional[int]:
-    _, target = find_start_end(map)
-    return dijstra2(map, target)
+    start = find_start(map, "E")
+
+    return dijstra(map, start, "a", can_walk_down)
 
 
 if __name__ == "__main__":
