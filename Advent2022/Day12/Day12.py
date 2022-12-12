d890486d4a62eb4cf1e4872c6c9d267b7def9068
Steve Burnap <steve@burnap.net>
2022-12-12 09:01:20 -0800

    Use Objects to make clearer and pass mypy

--- Advent2022/Day12/Day12.py
+++ Advent2022/Day12/Day12.py
@@ -3,28 +3,11 @@ from dataclasses import dataclass
 
 import aoc_utils as au
 
-Map = list[list[str]]
-
 
 @dataclass
 class Position:
     x: int
     y: int
-
-
-def adjacent(position: Position, map: Map) -> Generator[Position, None, None]:
-    if position.x > 0:
-        yield Position(position.x - 1, position.y)
-    if position.y > 0:
-        yield Position(position.x, position.y - 1)
-
-    if position.x < len(map[0]) - 1:
-        yield Position(position.x + 1, position.y)
-    if position.y < len(map) - 1:
-        yield Position(position.x, position.y + 1)
-
-
-WalkTest = Callable[[str, str], bool]
 
 
 class DikjstraData:
@@ -33,41 +16,77 @@ class DikjstraData:
             [None for _ in range(width)] for _ in range(height)
         ]
 
-    def get(self, x: int, y: int) -> Optional[int]:
-        return self.data[y][x]
+    def is_set(self, x: int, y: int) -> bool:
+        return self.data[y][x] is not None
+
+    def get(self, x: int, y: int) -> int:
+        if (rc := self.data[y][x]) is not None:
+            return rc
+        raise Exception("Attempt to read unset DikjstraData")
 
     def set(self, x: int, y: int, val: int):
         self.data[y][x] = val
 
 
+class Map:
+    def __init__(self, data: list[list[str]]):
+        self.data = data
+
+    def height(self):
+        return len(self.data)
+
+    def width(self):
+        return len(self.data[0])
+
+    def get(self, x: int, y: int) -> str:
+        return self.data[y][x]
+
+    def set(self, x: int, y: int, val: str):
+        self.data[y][x] = val
+
+
+def adjacent(position: Position, map: Map) -> Generator[Position, None, None]:
+    if position.x > 0:
+        yield Position(position.x - 1, position.y)
+    if position.y > 0:
+        yield Position(position.x, position.y - 1)
+
+    if position.x < map.width() - 1:
+        yield Position(position.x + 1, position.y)
+    if position.y < map.height() - 1:
+        yield Position(position.x, position.y + 1)
+
+
+WalkTest = Callable[[str, str], bool]
+
+
 def dijstra(
     map: Map, start: Position, target: str, can_walk: WalkTest
 ) -> Optional[int]:
-    dij: list[list[Optional[int]]] = [
-        [None for _ in range(len(map[0]))] for _ in range(len(map))
-    ]
+    dij = DikjstraData(map.width(), map.height())
 
-    dij[start.y][start.x] = 0
+    dij.set(start.x, start.y, 0)
+
     work_queue: list[Position] = [start]
 
     while len(work_queue) > 0:
         work = work_queue.pop(0)
         for adj in adjacent(work, map):
-            if dij[adj.y][adj.x] is None and can_walk(
-                map[work.y][work.x], map[adj.y][adj.x]
+            if not dij.is_set(adj.x, adj.y) and can_walk(
+                map.get(work.x, work.y), map.get(adj.x, adj.y)
             ):
-                dij[adj.y][adj.x] = dij[work.y][work.x] + 1
+                dij.set(adj.x, adj.y, dij.get(work.x, work.y) + 1)
                 work_queue.append(Position(adj.x, adj.y))
-                if map[adj.y][adj.x] == target:
-                    return dij[adj.y][adj.x]
+                if map.get(adj.x, adj.y) == target:
+                    return dij.get(adj.x, adj.y)
 
     return 0
 
 
 def find_start(map: Map, start: str) -> Position:
-    for y in range(len(map)):
-        for x in range(len(map[0])):
-            if map[y][x] == start:
+    for y in range(map.height()):
+        for x in range(map.width()):
+            if map.get(x, y) == start:
                 return Position(x, y)
 
     raise Exception("No starting position found")
@@ -87,11 +106,13 @@ def can_walk_up(first: str, second: str) -> bool:
     return ord(true_height(first)) + 1 >= ord(true_height(second))
 
 
-def test_one(map: Map) -> Optional[int]:
+def test_one(input: list[list[str]]) -> Optional[int]:
+    map = Map(input)
     return dijstra(map, find_start(map, "S"), "E", can_walk_up)
 
 
-def part_one(map: Map) -> Optional[int]:
+def part_one(input: list[list[str]]) -> Optional[int]:
+    map = Map(input)
     return dijstra(map, find_start(map, "S"), "E", can_walk_up)
 
 
@@ -99,11 +120,13 @@ def can_walk_down(first: str, second: str) -> bool:
     return ord(true_height(second)) + 1 >= ord(true_height(first))
 
 
-def test_two(map: Map) -> Optional[int]:
+def test_two(input: list[list[str]]) -> Optional[int]:
+    map = Map(input)
     return dijstra(map, find_start(map, "E"), "a", can_walk_down)
 
 
-def part_two(map: Map) -> Optional[int]:
+def part_two(input: list[list[str]]) -> Optional[int]:
+    map = Map(input)
     return dijstra(map, find_start(map, "E"), "a", can_walk_down)
 
 
